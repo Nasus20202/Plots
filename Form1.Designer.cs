b8a8e017526c8586881b7fa5047e8d40f1a9108c
Krzysztof Nasuta <krzysiek.com.pl@wp.pl>
2021-01-25 16:30:31 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿
+namespace Plots
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.formsPlot1 = new ScottPlot.FormsPlot();
+            this.equationText = new System.Windows.Forms.TextBox();
+            this.drawBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.fromText = new System.Windows.Forms.TextBox();
+            this.toText = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.stepText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // formsPlot1
+            // 
+            this.formsPlot1.Location = new System.Drawing.Point(12, 12);
+            this.formsPlot1.Name = "formsPlot1";
+            this.formsPlot1.Size = new System.Drawing.Size(893, 505);
+            this.formsPlot1.TabIndex = 0;
+            this.formsPlot1.Load += new System.EventHandler(this.formsPlot1_Load);
+            // 
+            // equationText
+            // 
+            this.equationText.Location = new System.Drawing.Point(123, 541);
+            this.equationText.Name = "equationText";
+            this.equationText.Size = new System.Drawing.Size(357, 20);
+            this.equationText.TabIndex = 1;
+            this.equationText.Text = "x^2 + (x + 2) - 9";
+            this.equationText.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // drawBtn
+            // 
+            this.drawBtn.Location = new System.Drawing.Point(825, 541);
+            this.drawBtn.Name = "drawBtn";
+            this.drawBtn.Size = new System.Drawing.Size(80, 23);
+            this.drawBtn.TabIndex = 2;
+            this.drawBtn.Text = "Rysuj";
+            this.drawBtn.UseVisualStyleBackColor = true;
+            this.drawBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 544);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Wpisz równanie:   y = ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(486, 546);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Od";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(595, 545);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Do";
+            // 
+            // fromText
+            // 
+            this.fromText.Location = new System.Drawing.Point(513, 542);
+            this.fromText.Name = "fromText";
+            this.fromText.Size = new System.Drawing.Size(76, 20);
+            this.fromText.TabIndex = 6;
+            this.fromText.Text = "0";
+            // 
+            // toText
+            // 
+            this.toText.Location = new System.Drawing.Point(622, 542);
+            this.toText.Name = "toText";
+            this.toText.Size = new System.Drawing.Size(80, 20);
+            this.toText.TabIndex = 7;
+            this.toText.Text = "10";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(708, 546);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Krok";
+            // 
+            // stepText
+            // 
+            this.stepText.Location = new System.Drawing.Point(743, 542);
+            this.stepText.Name = "stepText";
+            this.stepText.Size = new System.Drawing.Size(73, 20);
+            this.stepText.TabIndex = 9;
+            this.stepText.Text = "0,01";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(917, 583);
+            this.Controls.Add(this.stepText);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.toText);
+            this.Controls.Add(this.fromText);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.drawBtn);
+            this.Controls.Add(this.equationText);
+            this.Controls.Add(this.formsPlot1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Wykresy";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ScottPlot.FormsPlot formsPlot1;
+        private System.Windows.Forms.TextBox equationText;
+        private System.Windows.Forms.Button drawBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox fromText;
+        private System.Windows.Forms.TextBox toText;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox stepText;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Plots
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }   
+
+        private void formsPlot1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string equation = equationText.Text;
+            double from = 0, to = 0, step = 0;
+            try
+            {
+                from = Convert.ToDouble(fromText.Text);
+                to = Convert.ToDouble(toText.Text);
+                step = Convert.ToDouble(stepText.Text);
+                if (to <= from)
+                {
+                    MessageBox.Show("Wartość Do powinna być większa od wartości Od");
+                    return;
+                }
+                else if (step >= to - from)
+                {
+                    MessageBox.Show("Wartość Krok powinna być mniejsza od różnicy Od i Do");
+                    return;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Podaj prawidłowe wartości Od, Do i Krok");
+                return;
+            }
+            Stack<bool> brackets = new Stack<bool>();
+            foreach(char ch in equation)
+            {
+                if(ch == '(')
+                {
+                    brackets.Push(true);
+                }
+                if(ch == ')')
+                {
+                    if (brackets.Count>=1)
+                    {
+                        if (brackets.Peek() == true)
+                        {
+                            brackets.Pop();
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Jeden z nawiasów jest niedokmnięty");
+                        return;
+                    }
+                }
+            }
+            if (brackets.Count != 0)
+            {
+                MessageBox.Show("Jeden z nawiasów jest niedokmnięty");
+                return;
+            }
+            for(double i = from; i <=to; i+=step)
+            {
+                double x = i;
+                double y = calculate(equation, x);
+
+            }
+
+            double[] dataX = new double[] { 1, 2 };
+            double[] dataY = new double[] { 3, 4 };
+
+            formsPlot1.plt.PlotScatter(dataX, dataY);
+            formsPlot1.Render();
+        }
+
+        private double calculate(string equation, double x)
+        {
+            double y = 0; int bracketsOpened = 0;
+            List<string> parts = new List<string>();
+            string temp = String.Empty;
+            int c = 0;
+            foreach (char ch in equation)
+            {
+                c++;
+                if (ch == '(')
+                {
+                    bracketsOpened++;
+                    temp += ch;
+                }
+                else if (ch == ')')
+                {
+                    bracketsOpened--;
+                    temp += ch;
+                    if (temp != " " && temp != "" && bracketsOpened == 0)
+                    {
+                        
+                        parts.Add(temp);
+                        temp = String.Empty;
+                    }
+                    
+                   
+                }
+                else
+                {
+                    if (ch == ' ' && bracketsOpened == 0)
+                    {
+                        temp += ch;
+                        if (temp != " " && temp != "")
+                        {
+                            
+                            parts.Add(temp);
+                        }
+                        temp = String.Empty;
+                    }
+                    else if (c == equation.Length)
+                    {
+                        temp += ch;
+                        if (temp != " " && temp != "")
+                        {
+                            parts.Add(temp);
+                        }
+
+                    }
+                    else
+                    {
+                        temp += ch;
+                    }
+                }
+            }
+
+            for (int i = 0; i < parts.Count; i++)
+            {
+                string part = parts[i];
+                MessageBox.Show(part);
+            }
+
+            // Brackets
+            for (int i = 0; i < parts.Count; i++)
+            {
+                string part = parts[i];
+                if (part.Contains('(') && part.Contains(')'))
+                {
+                    String newPart = String.Empty;
+                    string newEquation = String.Empty;
+                    for (int j = 1; j < part.Length - 1; j++)
+                    {
+                        newEquation += part[j];
+                        
+                    }
+                    newPart = calculate(newEquation, x).ToString();
+                    MessageBox.Show(newEquation);
+                    parts[i] = newPart;
+                }
+            }
+            // x
+
+            for (int i = 0; i < parts.Count; i++)
+            {
+                string part = parts[i];
+                if (part.Contains("x"))
+                {
+                    string newPart = String.Empty;
+                    foreach(char ch in part)
+                    {
+                        if (ch != 'x') {
+                            newPart += ch;
+                        }
+                        else
+                        {
+                            newPart = x.ToString();
+                        }
+                    }
+                    parts[i] = newPart;
+                }
+            }
+
+            // Trigonometry
+
+
+
+            // Power, root
+
+            // Multiply, divide
+
+            // Add, subtract
+
+            for (int i = 0; i < parts.Count; i++)
+            {
+                string part = parts[i];
+                MessageBox.Show(part);
+            }
+            return y;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
